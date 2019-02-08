bae793615fc788fa482e2006d195f893428838e9
Scott Stadt <scott.stadt@gmail.com>
2019-02-08 10:16:39 -0500

    that whole 5 minutes of hard work sure paid off

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-import express from 'express'
+const express = require('express')
 
 const PORT = process.env.PORT || 3500
 const app = express()
@@ -7,3 +7,7 @@ const app = express()
 app.get('/', (req, res) => {
   res.send({ hello: 'there!' })
 })
+
+app.use('/static', express.static('files'))
+
+app.listen(3000)
